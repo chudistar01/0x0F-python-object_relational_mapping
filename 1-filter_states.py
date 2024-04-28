eef14df14d8b37601631d0d6101b0481ea23bbe6
chudistar01 <christianigwebuike31@gmail.com>
2024-04-28 02:48:02 -0700

    orm projects

--- 1-filter_states.py
+++ 1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         host="localhost", port=3306, user=sys.argv[1],
         password=sys.argv[2], database=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id""")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
